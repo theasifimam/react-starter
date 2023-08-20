a76b71563ff3803accbf79db15c112961a84c2c0
asif imam <asifimam999@gmail.com>
2023-08-20 13:07:57 +0530

    add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,47 @@
 import React, { useState } from "react";
 
 const Home = () => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [counter, setCounter] = useState(-1);
+  const [values, setValues] = useState({
+    input: " ",
+    error: 0,
+    score: 0,
+    index: 0,
+    performance: "",
+  });
+
   let content = "hello how are you? I am fine what about you?";
-  let contentArray = content.split("");
+  let wordArray = content.split(" ");
+  const charArray = wordArray.map((word, index) => {
+    return word.split("");
+  });
+
+  console.log(charArray);
 
   const handleChange = (e) => {
-    setEnteredValue(e.target.value);
-    setCounter(counter + 1);
+    setValues({ ...values, input: e.target.value, index: values.index + 1 });
+    const wordArray = values.value?.split(" ")?.map((word) => {
+      return word?.split("");
+    });
+    console.log(wordArray);
+
+    // let enteredChar = values.value
+    //   ? values.input.split("")[values.input.length - 1]
+    //   : values.input;
+
+    // values.input && contentArray[values.index] == enteredChar
+    //   ? setValues({ ...values, score: values.score + 1 })
+    //   : setValues({ ...values, error: values.error + 1 });
   };
-  function match() {
-    let enteredChar = enteredValue
-      ? enteredValue.split("")[enteredValue.length - 1]
-      : enteredValue;
-    console.log(contentArray[counter], enteredChar);
-  }
-  match();
 
   return (
     <div>
       <input type="text" name="content" id="content" onChange={handleChange} />
       <br />
-      {enteredValue}
+      {values.input}
       <br />
       {content}
+
+      <h1>{`Score : ${values.score} And Errors : ${values.error}`}</h1>
     </div>
   );
 };
